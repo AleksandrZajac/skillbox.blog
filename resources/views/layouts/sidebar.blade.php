def266b5b9579650a5d5881815c85860faf3386d
Aleksandr Zajac <mr.goodalex@gmail.com>
2021-09-02 22:42:43 +0300

    Fix: sidebar.blade.php

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,21 +1,8 @@
 <aside class="col-md-4 blog-sidebar">
     <div class="p-3">
         <h4 class="font-italic">Теги</h4>
-        <!-- <ol class="list-unstyled mb-0">
-            <li><a href="#">March 2014</a></li>
-            <li><a href="#">February 2014</a></li>
-            <li><a href="#">January 2014</a></li>
-            <li><a href="#">December 2013</a></li>
-            <li><a href="#">November 2013</a></li>
-            <li><a href="#">October 2013</a></li>
-            <li><a href="#">September 2013</a></li>
-            <li><a href="#">August 2013</a></li>
-            <li><a href="#">July 2013</a></li>
-            <li><a href="#">June 2013</a></li>
-            <li><a href="#">May 2013</a></li>
-            <li><a href="#">April 2013</a></li>
-        </ol> -->
-        @include('articles.tags', ['tags' => $tagsCloud])
+        <ol class="list-unstyled mb-0">
+            @include('articles.tags', ['tags' => $tagsCloud])
         </ol>
     </div>
-</aside><!-- /.blog-sidebar -->
+</aside>
