7494422afd342fdbdf69bd3ddc879aded04ed02a
Aleksandr Zajac <mr.goodalex@gmail.com>
2021-10-03 15:45:26 +0300

    Fix: contacts

--- app/Policies/ArticlePolicy.php
+++ app/Policies/ArticlePolicy.php
@@ -5,7 +5,6 @@ namespace App\Policies;
 use App\Models\Article;
 use App\Models\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
-use Illuminate\Auth\Access\Response;
 
 class ArticlePolicy
 {
